b7abe5622fc57f98d4de43d8f75bc34be0c44dfb
C Stavridis <castavridis@gmail.com>
2025-10-07 12:36:13 -0500

    Print preprocessed text

--- chp_2.py
+++ chp_2.py
@@ -1,4 +1,5 @@
 # Import "The Verdict" by Edith Warton into root
+# The Verdict is a 20,749-character short story with 4690 tokens
 import urllib.request
 url = ("https://raw.githubusercontent.com/rasbt/"
 "LLMs-from-scratch/main/ch02/01_main-chapter-code/"
@@ -8,14 +9,14 @@ urllib.request.urlretrieve(url, file_path)
 
 # Test that The Verdict has been downloaded and can be opened
 with open("the-verdict.txt", "r", encoding="utf-8") as f:raw_text = f.read()
-print("Total number of character:", len(raw_text))
-print(raw_text[:99])
+# print("Total number of character:", len(raw_text))
+# print(raw_text[:99])
 
 # Split text by white space
 import re
-test = "Hello, world! This is a test."
-result = re.split(r'([,.:;?_!"()\']|--|\s)', test)
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
 
 # Create an array from items that are not spaces
-result = [item.strip() for item in result if item.strip()]
-print(result)
+preprocessed = [item.strip() for item in preprocessed if item.strip()]
+# print(len(preprocessed))
+print(preprocessed[:30])
